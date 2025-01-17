7b180bb85fc6896874b084e75f09f4a12099afba
Esther <estherio555@yahoo.co.uk>
2025-01-17 00:15:13 -0500

    update main code

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from s3manager.s3_operations import download_file
 
 create_bucket("esther2026")
 
-upload_file("/Users/john_eze4u/Downloads/Loss.png", "esther2024")
+upload_file("/Users/john_eze4u/Downloads/Billing.png", "esther2024")
 
 delete_bucket("Esther2026")
 
-upload_file(file_path="Loss.png", bucket_name="esther2026", object_name="projects/dev/esther2026.png")
+upload_file(file_path="Billing.png", bucket_name="esther2026", object_name="projects/dev/esther2026.png")
 
